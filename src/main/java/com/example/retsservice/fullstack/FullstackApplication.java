dd529b15d2811a0db6151720ee5d8482171a7880
Sk122521 <51444727+Sk122521@users.noreply.github.com>
2021-10-10 13:56:10 +0530

    Update FullstackApplication.java

--- src/main/java/com/example/retsservice/fullstack/FullstackApplication.java
+++ src/main/java/com/example/retsservice/fullstack/FullstackApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan({"com.example.retsservice.fullstack.controller"})
 public class FullstackApplication {
 
 	public static void main(String[] args) {
